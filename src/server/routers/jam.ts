55091aff344138048d23f19fd20e3999c043eed9
Zachary Ho <zyyho97@gmail.com>
2023-01-28 00:15:07 +1100

    made new schema

--- src/server/routers/jam.ts
+++ src/server/routers/jam.ts
@@ -1,5 +1,14 @@
-import { createRouter } from '~/server/createRouter';
+import { createRouter } from 'server/createRouter';
 import { z } from 'zod';
+
+const Visibility = ['public', 'private'] as const;
+
+// Sample domain object declaration and class object shape
+const createJamRequest = z.object({
+  name: z.string(),
+  details: z.string(),
+  visibility: z.enum(Visibility)
+});
 
 export const jamRouter = createRouter()
   .query('', {
