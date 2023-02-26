4227ccdf7d8bb91c16e8a364087b8b84eee806b4
Zachary Ho <zyyho97@gmail.com>
2023-02-26 23:28:34 +1100

    added comments to exported route handlers

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,14 +2,17 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { createContext } from 'server/trpc/context';
 import { appRouter } from 'server/routers/_app';
 
-const handler = (request: Request) => 
-  fetchRequestHandler({
+const handler = (request: Request) => {
+  console.log('handler called with request', request);
+  return fetchRequestHandler({
     endpoint: '/api/trpc',
     req: request,
     router: appRouter,
     createContext
   });
-;
+};
 
+// Queries
 export const GET = handler;
+// Mutations
 export const POST = handler;
