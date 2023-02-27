3188c19477787fa29ef47ffd869a946421bee8f6
Zachary Ho <zyyho97@gmail.com>
2023-02-27 23:43:46 +1100

    removed trpc testing code from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,12 @@ import * as React from 'react';
 import { trpc } from 'app/trpcClient';
 
 const Home = () => {
-  const jamCreate = trpc.jam.create.useMutation();
-
-  const handleButtonClicked = () => {
-    jamCreate.mutate({
-        'id': 'c8b0d935-dfd3-490a-a35f-329c1e1eb67a',
-        'name': 'test new jam',
-        'startDatetime': '2023-02-19 22:20:46',
-        'endDatetime': '2023-02-19 22:20:48',
-        'visibility': 'PRIVATE'
-    });
-  };
+  const [placeholder, setPlaceholder] = React.useState(undefined);
 
   return (
     <main>
-      <h1> Jammin&apos; </h1>
-      <button type='button' onClick={handleButtonClicked}>make jam</button>
+      <h1>Jammin&apos;</h1>
+      <button type='button'>make jam</button>
     </main>
   );
 };
